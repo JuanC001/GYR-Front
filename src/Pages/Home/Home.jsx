e726f4144b45a40b56380c7904719fa36c493bda
Juan Camilo <flopa.j@outlook.com>
2024-05-06 13:11:16 -0500

    HERO: Load Images Fixes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,9 +5,12 @@ import { motion, useScroll, useTransform } from 'framer-motion'
 import FrontLayer from '../../assets/hero/Front-Layer.webp'
 import BackLayer from '../../assets/hero/Back-Layer.webp'
 
-import { Suspense, useRef } from 'react'
+import { Suspense, useRef, useState } from 'react'
 
 export const Home = () => {
+
+  const [frontLoaded, setFrontLoaded] = useState(false);
+  const [backLoaded, setBackLoaded] = useState(false);
 
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
@@ -19,26 +22,30 @@ export const Home = () => {
   const translation_back = useTransform(scrollYProgress, [0, 1], ["0%", "30%"]);
   const translation_front = useTransform(scrollYProgress, [0, 1], ["0%", "10%"]);
 
+
   return (
     <main className='home-main'>
 
       <div ref={ref} className="hero-container">
-        <motion.h1 style={{ y: translation_text }} initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.5 } }} className='hero-tittle'>
+        <motion.h1 style={{ y: translation_text }} initial={{ opacity: 0 }} animate={(frontLoaded && backLoaded) && { opacity: 1, transition: { delay: 0.5 } }} className='hero-tittle'>
           Gestión y Resultados
         </motion.h1>
         <Suspense fallback={<div>Loading...</div>}>
           <motion.img className='hero-front'
             initial={{ opacity: 0, y: 100 }}
-            animate={{ opacity: 1, y: 0, transition: { delay: 0, bounce: false } }}
+            animate={frontLoaded && { opacity: 1, y: 0, transition: { delay: 0, bounce: false } }}
             src={FrontLayer} alt='Layer_5'
             style={{ y: translation_front }}
-            loading='lazy' />
+            loading='lazy'
+            onLoad={() => setFrontLoaded(true)} />
           <motion.img className='hero-back'
             initial={{ opacity: 0, y: 100 }}
-            animate={{ opacity: 1, y: 0, transition: { delay: 0.2, bounce: false } }}
+            animate={backLoaded && { opacity: 1, y: 0, transition: { delay: 0.2, bounce: false } }}
             src={BackLayer} alt='Layer_1'
             style={{ y: translation_back }}
-            loading='lazy' />
+            loading='lazy'
+            onLoad={() => setBackLoaded(true)} />
+
         </Suspense>
       </div>
     </main>
