f3697d9379c94ed21bb3c1c934673154bbcbbe81
Juan Camilo <flopa.j@outlook.com>
2024-07-03 17:57:12 -0500

    Seccion de clientes terminada

--- src/assets/home/images/Customer/imagesExport.js
+++ src/assets/home/images/Customer/imagesExport.js
@@ -24,96 +24,126 @@ export const ImageGallery = [
         id: 1,
         image: Artboard1,
         title: 'Air-e',
+        group: 1
     },
     {
         id: 2,
         image: Artboard2,
         title: 'ADS',
+        group: 1
     },
     {
         id: 3,
         image: Artboard3,
         title: 'Aguas Bolivar',
+        group: 1
     },
     {
         id: 4,
         image: Artboard4,
         title: 'Aguas de manizales',
+        group: 1
+
     },
     {
         id: 5,
         image: Artboard5,
         title: 'Aguas Kpital Cúcuta',
+        group: 1
+
     },
     {
         id: 6,
         image: Artboard6,
         title: 'edesa',
+        group: 1
+
     },
     {
         id: 7,
         image: Artboard7,
         title: 'EMCASERVICIOS',
+        group: 1
+
     },
     {
         id: 8,
         image: Artboard8,
         title: 'Empitalito',
+        group: 1
     },
     {
         id: 9,
         image: Artboard9,
         title: 'Emserchía',
+        group: 2
     },
     {
         id: 10,
         image: Artboard10,
         title: 'EMSERFUSA ESP',
+        group: 2
+
     },
     {
         id: 11,
         image: Artboard11,
         title: 'Interaseo S.A.E.S.P',
+        group: 2
+
     },
     {
         id: 12,
         image: Artboard12,
         title: 'Emset s.a',
+        group: 2
+
     },
     {
         id: 13,
         image: Artboard13,
         title: 'Aguas del Magdalena',
+        group: 2
+
     },
     {
         id: 14,
         image: Artboard14,
         title: 'PAP-PDA Norte de Santander',
+        group: 2
+
     },
     {
         id: 15,
         image: Artboard15,
         title: 'Vallecaucana de Aguas S.A.E.S.P',
+        group: 2
+
     },
     {
         id: 16,
         image: Artboard16,
         title: 'Habitat Limpio',
+        group: 2
+
     },
     {
         id: 17,
         image: Artboard17,
         title: 'Aguas de Barrancabermeja S.A.E.S.P',
+        group: 3
     },
     {
         id: 18,
         image: Artboard18,
         title: 'Aguas del Cesar',
+        group: 3
     },
     {
         id: 19,
         image: Artboard19,
         title: 'AAA de la Jagua de Iberico',
+        group: 3
     }
-    
+
 ]
