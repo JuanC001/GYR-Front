635c1791dfd826f76cfaed8b374e15eed09382a6
Juan Camilo <flopa.j@outlook.com>
2024-07-05 10:10:19 -0500

    general fixes

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -7,24 +7,30 @@ export const Services = () => {
   return (
     <div>
 
-      <section className="services-section">
-        <div className="services-landing">
-          <h1>Nuestros <span>Servicios</span></h1>
-        </div>
-
-      </section>
+      <div className="services-landing">
+        <h1>Nuestros <span>Servicios</span></h1>
+      </div>
 
       <section className="services-section-intro">
         <div className="decoration-services-container">
           <img src={DecorationSVG} />
         </div>
         <div className="services-intro-container">
-          <Consultorias />
+          <p>
+            Desarrollamos auditorías externas, interventorías y actividades asociadas al fortalecimiento institucional y estructuración de alternativas empresariales eficientes, a prestadores de servicios públicos domiciliarios, nacional e internacionalmente.
+          </p>
         </div>
       </section>
 
       <section className="services-section">
+        <h2>
+          Consultorías
+        </h2>
+        <Consultorias />
+      </section>
 
+      <section className="services-section">
+        
       </section>
 
     </div>
