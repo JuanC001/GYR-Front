ac6ad737bacf22b0c9347e71bd4abb81a019cebc
Juan Camilo <flopa.j@outlook.com>
2024-07-09 20:44:44 -0500

    map advance 1

--- src/Pages/Experience/Experience.jsx
+++ src/Pages/Experience/Experience.jsx
@@ -1,11 +1,31 @@
 import { Map } from "./components/Map/Map"
+import './Experience.css'
 
+import DecorationSVG from '../../../src/assets/about/images/decoration.svg'
 
 export const Experience = () => {
     return (
-        <div>
-            <h1>Experience</h1>
-            <Map />
-        </div>
+        <main className="experience__container">
+            <div className="experience__container_landing">
+                <h1>Nuestra <span>Experiencia</span></h1>
+            </div>
+            <section className="experience__description">
+                <div className="decoration-about-container">
+                    <img src={DecorationSVG} />
+                </div>
+                <div className="experience_description_content">
+                    <h2>
+                        Nuestra Trayectoria
+                    </h2>
+
+                    <p>
+                        En Gestión y Resultados S.A.S. contamos con más de <span>19 AÑOS</span> de experiencia y participación en más de <span>160 PROYECTOS</span> tanto a nivel nacional como internacional.
+                    </p>
+                </div>
+            </section>
+            <section className="experience__section">
+                <Map />
+            </section>
+        </main>
     )
 }
