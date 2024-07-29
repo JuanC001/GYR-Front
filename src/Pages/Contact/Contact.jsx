0ce76aa33402871ca3a4f43eaa3d3b881c942486
Juan Camilo <flopa.j@outlook.com>
2024-07-28 22:02:47 -0500

    Cambios en el spinner de contacto

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -4,6 +4,9 @@ import './Contact.css'
 
 import DecorationSVG from '../../assets/contact/decoration.svg'
 import { useState } from 'react'
+import LoaderSVG from '../../assets/loader.svg'
+import CheckSVG from '../../assets/check.svg'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export const Contact = () => {
   const { sendEmail } = useEmail()
@@ -12,12 +15,29 @@ export const Contact = () => {
   const [asunto, setAsunto] = useState('')
   const [correo, setCorreo] = useState('')
   const [mensaje, setMensaje] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [loaded, setLoaded] = useState(false)
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    alert('El mensaje ha sido enviado correctamente.')
     // Enviar email aquí
-    sendEmail(nombre, correo, asunto, mensaje)
+    console.log("Sending email...")
+    setLoading(true)
+    const res = await sendEmail(nombre, correo, asunto, mensaje)
+    if (res === "Error") {
+      alert("Ha ocurrido un error! Notifique al administrador")
+      setLoading(false)
+      return
+    }
+    setLoading(false)
+    setLoaded(true)
+    setTimeout(animationEnd, 2000)
+  }
+
+  const animationEnd = () => {
+
+    console.log("Animation")
+    setLoaded(false)
   }
 
   return (
@@ -42,7 +62,20 @@ export const Contact = () => {
           </p>
         </section>
         <section className="contact-section">
+
           <form onSubmit={handleSubmit}>
+            <AnimatePresence>
+              {loading && <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className='spinner'>
+                <motion.img animate={{ rotate: [0, 360, 0] }} transition={{ repeat: Infinity, duration: 1 }} src={LoaderSVG} alt='Cargando...' />
+              </motion.div>}
+              {loaded && <motion.div className='spinner' initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                <motion.img src={CheckSVG} initial={{ rotate: 180 }} animate={{ rotate: 0 }} exit={{ rotate: 360 }} >
+                </motion.img>
+                <p>El mensaje ha sido enviado correctamente.</p>
+              </motion.div>}
+
+            </AnimatePresence>
+
             <input type="text" name='name' placeholder="Nombre" required onChange={(e) => setNombre(e.target.value)} />
             <input type="text" name='asunto' placeholder="Asunto" required onChange={(e) => setAsunto(e.target.value)} />
             <input type="email" name='email' placeholder="Correo electrónico" required onChange={(e) => setCorreo(e.target.value)} />
@@ -68,6 +101,6 @@ export const Contact = () => {
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11246.850326658123!2d-74.07412872080178!3d4.707011851017213!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9accf61190e1%3A0x717d458a4f827863!2sCl.%20122%20%2352-49%2C%20Bogot%C3%A1!5e0!3m2!1ses!2sco!4v1722209160114!5m2!1ses!2sco" width="100%" height="500" style={{ border: 0 }} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </section>
       </div>
-    </main>
+    </main >
   )
 }
